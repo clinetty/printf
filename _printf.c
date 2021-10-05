10f97bbedc4357d0ef3e34010e5c47bad67c653c
clinetty <clintogechi@gmail.com>
2021-10-05 17:00:35 +0300

    Added _printf.c

--- _printf.c
+++ _printf.c
@@ -29,6 +29,7 @@ int _printf(const char *format, ...)
 		return (-1);
 
 	va_start(arg_list, format);
+	/*calling parser function*/
 	printed_chars = parser(format, f_list, arg_list);
 	va_end(arg_list);
 	return (printed_chars);
